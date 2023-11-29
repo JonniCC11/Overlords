f70eb79f562980479490d12be12918f849a94693
¨JonniCC¨ <jonatanangeluz@gmail.com>
2023-11-29 18:33:45 -0300

    Se agrega cambios requeridos en el tp

--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,20 @@
 const icono = document.getElementById('icono');
 
 icono.addEventListener('click', function() {
-    // Lógica para manejar el clic del icono
+
 });
 
 
 
-// Obtén el elemento de audio
+
 var musica = document.getElementById("musica");
 
-// Reproduce la música automáticamente cuando se carga la página
+
 window.onload = function() {
   musica.play();
 };
 
-// Función para pausar o reanudar la música cuando el usuario hace clic en un botón (por ejemplo)
+
 function toggleMusica() {
   if (musica.paused) {
     musica.play();
